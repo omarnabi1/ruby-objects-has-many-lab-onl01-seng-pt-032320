23951925cb44366ff873a341481efcaad587a804
Omar Nabi <onabi20@gmail.com>
2020-04-20 03:46:08 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+class Post
+  attr_accessor :title, :author 
+
+  @@all = []
+
+  def initialize(name, artist=nil)
+    @name = name
+    @artist = artist
+    @@all << self
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def self.all
+    @@all
+  end
+end
